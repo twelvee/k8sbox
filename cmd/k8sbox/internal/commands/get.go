ecd9fbf399e4f8e8932cfd81d026f9d3a05a759e
Sergey Ziryanov <redapple54r@gmail.com>
2023-05-23 17:12:19 +0700

    fix: build get command

--- cmd/k8sbox/internal/commands/get.go
+++ cmd/k8sbox/internal/commands/get.go
@@ -8,13 +8,14 @@ import (
 func NewGetCommand() *cobra.Command {
 	var (
 		command *cobra.Command
+		flags   []string
 	)
 	command = &cobra.Command{
 		Use:   "get",
 		Short: "Get saved environments",
 		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			handlers.HandleGetCommand(args[0], command.Context())
+			handlers.HandleGetCommand(args[0], flags, command.Context())
 			return nil
 		},
 	}
